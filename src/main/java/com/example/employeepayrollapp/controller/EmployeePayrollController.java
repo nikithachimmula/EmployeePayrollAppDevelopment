712a6719be2575f1716e0aabf04a2ed7f79f2ddc
nikitha chimmmula <nikithachimmula97@gmail.com>
2022-07-29 16:38:07 +0530

    Add: EmployeePayrollSpringApp

---
+++ src/main/java/com/example/employeepayrollapp/controller/EmployeePayrollController.java
@@ -0,0 +1,47 @@
+package com.example.employeepayrollapp.controller;
+
+import com.example.employeepayrollapp.dto.EmployeePayrollDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employeepayrollservice")
+public class EmployeePayrollController {
+
+    @RequestMapping(value = {"", "/", "/get"})
+    public ResponseEntity<String> getEmployeePayrollData() {
+
+        return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
+
+    }
+
+    @GetMapping("/get/{empId}")
+    public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId") int empId) {
+
+        return new ResponseEntity<String>("Get Call Success for id: " + empId, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
+
+        return new ResponseEntity<String>("Created Employee Payroll Data for: " + empPayrollDTO, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/update")
+
+    public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
+
+        return new ResponseEntity<String>("Updated Employee Payroll Data for: " + empPayrollDTO, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/delete/{empId}")
+
+    public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
+
+        return new ResponseEntity<String>("Delete Call Success for id: " + empId, HttpStatus.OK);
+    }
+}
