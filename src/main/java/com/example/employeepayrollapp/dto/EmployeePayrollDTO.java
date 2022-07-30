f55d8f3317bf7810812f917de213bbcaa897121f
nikitha chimmmula <nikithachimmula97@gmail.com>
2022-07-30 22:19:20 +0530

    Adding_more_properties

--- src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,14 +1,24 @@
 package com.example.employeepayrollapp.dto;
 
+import lombok.ToString;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
-public class EmployeePayrollDTO {
+public @ToString class EmployeePayrollDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     public String name;
-    @Min(value=500,message="Min wage should be more than 500")
+    @Min(value = 500, message = "Min wage should be more than 500")
     public long salary;
+
+    public String gender;
+    public LocalDate startDate;
+    public String note;
+    public String profilepic;
+    public List<String> department;
 
     public EmployeePayrollDTO(String name, long salary) {
         this.name = name;
