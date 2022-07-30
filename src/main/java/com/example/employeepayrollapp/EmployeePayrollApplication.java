6e6957f1e7ccbe64691f96067efe22da91959d92
nikitha chimmmula <nikithachimmula97@gmail.com>
2022-07-30 12:18:53 +0530

    LombokJarFiles

---
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollApplication.java
@@ -0,0 +1,20 @@
+package com.example.employeepayrollapp;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+@Slf4j
+public class EmployeePayrollApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+        log.info("Employee Payroll Application Started in {} Environment", context.getEnvironment().getProperty("environment"));;
+    }
+
+}
