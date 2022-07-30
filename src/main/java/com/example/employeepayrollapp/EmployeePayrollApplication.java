8f43d91140894be37bfae2590e42389db45526da
nikitha chimmmula <nikithachimmula97@gmail.com>
2022-07-30 13:48:17 +0530

    DEV_PROD_Environment

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollApplication.java
@@ -7,10 +7,10 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
-public class EmployeePayrollAppApplication {
+public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
         log.info("Employee Payroll Application Started in {} Environment", context.getEnvironment().getProperty("environment"));
     }
 
